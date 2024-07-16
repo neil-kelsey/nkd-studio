2c630347f782b4e54dac591b8e84fda4565d5de1
Neil Kelsey <neil.kelsey@gmail.com>
2024-07-16 14:41:14 +0100

    feat: Adding Typewriter effect to Hero section

--- nkd-studio/src/app/components/hero.tsx
+++ nkd-studio/src/app/components/hero.tsx
@@ -1,10 +1,120 @@
+'use client';
+
 import styles from "./hero.module.css";
+import Typewriter from 'typewriter-effect';
 
 export default function Hero() {
   return (
     <article className={styles.hero}>
-      <span className={styles.nkd}>Neil Kelsey Development</span>
+      <span className={styles.nkd}>
+        <Typewriter
+          options={{
+            loop: true,
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("Neil Kelsey Development")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("Next-Gen Knowledge Development")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("Neil Kelsey Development")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("Networked Know-how Development")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("Neil Kelsey Development")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("New Key Development")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("Neil Kelsey Development")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("Navigating Kaleidoscope Dream")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("Neil Kelsey Development")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("Noble Knight Development")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("Neil Kelsey Development")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("NKD")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .typeString("Not Known Development")
+              .pauseFor(2000)
+              .deleteAll()
+              .pauseFor(500)
+              .start();
+          }}
+        />
+      </span>
       <span className={styles.studio}>Studio</span>
+      
     </article>
   );
 }
